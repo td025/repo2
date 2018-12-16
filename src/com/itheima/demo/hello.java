00c2e16756024eb5ffc34473d70a49411bca0627
taodong <taodongwork123@163.com>
2018-12-16 21:24:01 +0800

    添加到本地

---
+++ src/com/itheima/demo/hello.java
@@ -0,0 +1,9 @@
+package com.itheima.demo;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("我是一只等待千年白狐.");
+        System.out.println("千年等待千年孤独.");
+        System.out.println("人在灯火阑珊处观看");
+    }
+}
